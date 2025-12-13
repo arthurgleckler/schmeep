e79907d10fdc989fe203a51d8975411c5b89ff35
Arthur A. Gleckler <aag@alum.mit.edu>
2025-12-12 16:05:07 -0800

    Refactor.  Combine let's and let*'s.

--- lib/eg.scm
+++ lib/eg.scm
@@ -15,31 +15,30 @@
      (lambda (event-json)
        (let* ((expr-list '(expression ...))
               (quoted-expr (cons 'begin expr-list))
-              (port (open-output-string)))
-         (let ((result
-                (parameterize ((current-output-port port))
-                  (call-with-current-continuation
-                   (lambda (k)
-                     (with-exception-handler k
-                       (lambda ()
-                         (eval quoted-expr (interaction-environment)))))))))
-           (let* ((output-str (get-output-string port))
-                  (expr-sxml `(code ,(call-with-output-string
-				      (lambda (p) (write quoted-expr p)))))
-                  (result-sxml (if (exception? result)
-                                   `(span (@ (class "error"))
-					  ,(format-exception result ""))
-                                   `(code ,(call-with-output-string
-					    (lambda (p) (write result p))))))
-                  (sxml (if (string=? output-str "")
-                            `(li ,expr-sxml " ⇒ " ,result-sxml)
-                            `(li ,expr-sxml
-				 (br)
-				 (span (@ (class "output")) ,output-str)
-				 " ⇒ "
-				 ,result-sxml)))
-                  (html-content (sxml->xml sxml)))
-             (rax-response "#scheme-content ul" "append" html-content))))))))
+              (port (open-output-string))
+	      (result
+               (parameterize ((current-output-port port))
+                 (call-with-current-continuation
+                  (lambda (k)
+                    (with-exception-handler k
+                      (lambda ()
+                        (eval quoted-expr (interaction-environment))))))))
+	      (output-str (get-output-string port))
+              (expr-sxml `(code ,(call-with-output-string
+				  (lambda (p) (write quoted-expr p)))))
+              (result-sxml (if (exception? result)
+                               `(span (@ (class "error"))
+				      ,(format-exception result ""))
+                               `(code ,(call-with-output-string
+					(lambda (p) (write result p))))))
+              (sxml (if (string=? output-str "")
+                        `(li ,expr-sxml " ⇒ " ,result-sxml)
+                        `(li ,expr-sxml
+			     (br)
+			     (span (@ (class "output")) ,output-str)
+			     " ⇒ "
+			     ,result-sxml))))
+         (rax-response "#scheme-content ul" "append" (sxml->xml sxml)))))))
 
 (define (eg-clear event-json)
   (rax-response "#scheme-content ul" "replace" "<ul></ul>"))
